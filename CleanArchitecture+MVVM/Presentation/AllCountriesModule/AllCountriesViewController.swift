ae0ccb1f661932e619f78a4e287f3c7d6eeb6273
torresrodrigo <33295900+torresrodrigo@users.noreply.github.com>
2024-07-10 15:14:42 -0400

    Add viewModel and ui (#4)

--- CleanArchitecture+MVVM/Presentation/AllCountriesModule/AllCountriesViewController.swift
+++ CleanArchitecture+MVVM/Presentation/AllCountriesModule/AllCountriesViewController.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class AllCountriesViewController: UIViewController {
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.frame = self.view.bounds
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     let viewModel: AllCountriesViewModel
+    
+    var countries: [Country] = []
+    var isError: Bool = false
     
     init(viewModel: AllCountriesViewModel) {
         self.viewModel = viewModel
@@ -22,8 +36,21 @@ class AllCountriesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        self.view.addSubview(tableView)
+        setupContraints()
+        
         getDataUseCase()
+    }
+    
+    
+    private func setupContraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
     
     
@@ -31,13 +58,41 @@ class AllCountriesViewController: UIViewController {
         Task {
             do {
                 let resultCountries = await viewModel.getAllCountries()
-                print(resultCountries)
+                countries = resultCountries
+                tableView.reloadData()
             } catch {
                 print(error)
+                isError = true
             }
         
         }
     }
 
+}
+
+extension AllCountriesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if isError  {
+            return 1
+        }
+        
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        if isError {
+            cell.textLabel?.text = "Tuvimos un error para obtener la información"
+            return cell
+        }
+        
+        let modelCountries = countries
+        cell.textLabel?.text = "\(modelCountries[indexPath.row].name)"
+        return cell
+    }
+    
+    
 }
 
